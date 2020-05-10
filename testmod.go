859b88ed98167535598b1e0704c42ce025c8a424
Mithun Shashidhara <sashidm@wz.hasbro.com>
2020-05-10 16:14:59 +0530

    1.0.5

--- testmod.go
+++ testmod.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // Hi returns a friendly greeting
 func Hi(name string) string {
-	fmt.Println("golang version !!!!")
-   return fmt.Sprintf("MSG from golangv1.0.4:  %s", name)
+	fmt.Println("hello world !!!!")
+   return fmt.Sprintf("MSG from golangv1.0.5:  %s", name)
 }
 
